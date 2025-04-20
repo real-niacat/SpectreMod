7b87563dc6f3d3573d9e19c111dbfaa9fa2275e3
dragokillfist <tinker198431@gmail.com>
2025-04-19 20:37:59 -0500

    added all the pre-HM drops conditions and a placeholder texture

--- Core/DropConditions/CharmDrops.cs
+++ Core/DropConditions/CharmDrops.cs
@@ -25,4 +25,112 @@ namespace SpectreMod.Core.DropConditions
             return Description.Value;
         }
     }
+    public class EoCFragmentDropCondition : IItemDropRuleCondition
+    {
+        private static LocalizedText Description;
+
+        public EoCFragmentDropCondition() {
+            Description ??= Language.GetOrRegister("Mods.SpectreMod.DropConditions.EoCCharm");
+        }
+
+        public bool CanDrop(DropAttemptInfo info) {
+            return Main.expertMode &! NPC.downedBoss1;
+        }
+        public bool CanShowItemDropInUI() {
+            return true;
+        }
+        public string GetConditionDescription() {
+            return Description.Value;
+        }
+    }
+    public class EoWFragmentDropCondition : IItemDropRuleCondition
+    {
+        private static LocalizedText Description;
+
+        public EoWFragmentDropCondition() {
+            Description ??= Language.GetOrRegister("Mods.SpectreMod.DropConditions.EoWCharm");
+        }
+
+        public bool CanDrop(DropAttemptInfo info) {
+            return Main.expertMode &! NPC.downedBoss2;
+        }
+        public bool CanShowItemDropInUI() {
+            return true;
+        }
+        public string GetConditionDescription() {
+            return Description.Value;
+        }
+    }
+    public class SkeletronFragmentDropCondition : IItemDropRuleCondition
+    {
+        private static LocalizedText Description;
+
+        public SkeletronFragmentDropCondition() {
+            Description ??= Language.GetOrRegister("Mods.SpectreMod.DropConditions.SkeletronCharm");
+        }
+
+        public bool CanDrop(DropAttemptInfo info) {
+            return Main.expertMode &! NPC.downedBoss3;
+        }
+        public bool CanShowItemDropInUI() {
+            return true;
+        }
+        public string GetConditionDescription() {
+            return Description.Value;
+        }
+    }
+    public class QueenBeeFragmentDropCondition : IItemDropRuleCondition
+    {
+        private static LocalizedText Description;
+
+        public QueenBeeFragmentDropCondition() {
+            Description ??= Language.GetOrRegister("Mods.SpectreMod.DropConditions.QueenBeeCharm");
+        }
+
+        public bool CanDrop(DropAttemptInfo info) {
+            return Main.expertMode &! NPC.downedQueenBee;
+        }
+        public bool CanShowItemDropInUI() {
+            return true;
+        }
+        public string GetConditionDescription() {
+            return Description.Value;
+        }
+    }
+    public class DeerClopsFragmentDropCondition : IItemDropRuleCondition
+    {
+        private static LocalizedText Description;
+
+        public DeerClopsFragmentDropCondition() {
+            Description ??= Language.GetOrRegister("Mods.SpectreMod.DropConditions.DeerClopsCharm");
+        }
+
+        public bool CanDrop(DropAttemptInfo info) {
+            return Main.expertMode &! NPC.downedDeerclops;
+        }
+        public bool CanShowItemDropInUI() {
+            return true;
+        }
+        public string GetConditionDescription() {
+            return Description.Value;
+        }
+    }
+    public class WoFFragmentDropCondition : IItemDropRuleCondition
+    {
+        private static LocalizedText Description;
+
+        public WoFFragmentDropCondition() {
+            Description ??= Language.GetOrRegister("Mods.SpectreMod.DropConditions.WoFCharm");
+        }
+
+        public bool CanDrop(DropAttemptInfo info) {
+            return Main.expertMode &! Main.hardMode;
+        }
+        public bool CanShowItemDropInUI() {
+            return true;
+        }
+        public string GetConditionDescription() {
+            return Description.Value;
+        }
+    }
 }
