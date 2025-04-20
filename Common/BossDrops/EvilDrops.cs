a159dbb5a8391189b1493d2e0ff0d3c92104ffb6
dragokillfist <tinker198431@gmail.com>
2025-04-19 22:57:48 -0500

    adds the evil and EoC fragment to the drops and adds some more locs

---
+++ Common/BossDrops/EvilDrops.cs
@@ -0,0 +1,18 @@
+using SpectreMod.Core.DropConditions;
+using SpectreMod.Content.CharmsFragments;
+using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ModLoader;
+
+namespace SpectreMod.Common.BossDrops
+{
+    public class EvilDrops : GlobalNPC
+    {
+        public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot) {
+            if (npc.type == NPCID.EaterofWorldsTail || npc.type == NPCID.BrainofCthulhu) {
+                npcLoot.Add(ItemDropRule.ByCondition(new EvilFragmentDropCondition(), ModContent.ItemType<EvilFragment>(), 1, 1, 1));
+            }
+        }
+    }
+}
---
+++ Common/BossDrops/EyeOfCthulhuDrops.cs
@@ -0,0 +1,18 @@
+using SpectreMod.Core.DropConditions;
+using SpectreMod.Content.CharmsFragments;
+using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ModLoader;
+
+namespace SpectreMod.Common.BossDrops
+{
+    public class EyeOfCthulhuDrops : GlobalNPC
+    {
+        public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot) {
+            if (npc.type == NPCID.EyeofCthulhu) {
+                npcLoot.Add(ItemDropRule.ByCondition(new EoCFragmentDropCondition(), ModContent.ItemType<EoCFragment>(), 1, 1, 1));
+            }
+        }
+    }
+}
