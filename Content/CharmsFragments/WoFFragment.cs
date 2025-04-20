ed5f8d0806400a21e8705f675b9b8a0eb074b042
dragokillfist <tinker198431@gmail.com>
2025-04-20 10:55:54 -0500

    and now charm fragments use new placeholder

--- Content/CharmsFragments/WoFFragment.cs
+++ Content/CharmsFragments/WoFFragment.cs
@@ -12,7 +12,7 @@ namespace SpectreMod.Content.CharmsFragments
             Item.maxStack = 1;
             Item.value = Item.sellPrice(0, 1);
         }
-        public override string Texture => "SpectreMod/Common/PlaceHolders/PlaceHolder";
+        public override string Texture => "SpectreMod/Common/PlaceHolders/CharmFragmentPlaceholder";
 
         public override void AddRecipes()
         {
