73c817fb2e8c899053d24b8418946ce09dceff30
normalcat <tntgav@gmail.com>
2025-04-19 19:39:59 -0700

    ideally adds the tier 2 and 3 distraught charms working

---
+++ Content/Charms/CharmDistraught_UpgradedPlus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria;
+
+namespace SpectreMod.Content.Charms
+{
+    public class CharmDistraught_UpgradedPlus : ModItem
+    {
+        public override string Texture => "SpectreMod/Common/PlaceHolders/PlaceHolder";
+
+        public override void SetDefaults()
+        {
+            //this is copied code from myself
+            Item.width = 32;
+            Item.height = 32;
+            Item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<CharmDistraughtPlayer>().intensity = 3;
+        }
+    }
+}
