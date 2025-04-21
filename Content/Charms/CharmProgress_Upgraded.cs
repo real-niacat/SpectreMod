d3c55b903ffe117c80eb76477f45a970ade30f9b
normalcat <tntgav@gmail.com>
2025-04-20 17:28:55 -0700

    add recipes to upgraded charms, remove magmating debuff texture for now

--- Content/Charms/CharmProgress_Upgraded.cs
+++ Content/Charms/CharmProgress_Upgraded.cs
@@ -27,5 +27,14 @@ namespace SpectreMod.Content.Charms
             player.GetDamage(DamageClass.Generic) += 0.2f; //+10% damage
             player.GetModPlayer<CharmProgressPlayer>().intensity = 20; //10% boost to accel speed
         }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient<CharmProgress_Base>();
+            recipe.AddIngredient(ItemID.LunarBar, 25);
+            recipe.AddTile(TileID.LunarCraftingStation);
+            recipe.Register();
+        }
     }
 }
