d249e527a7f4132ba15fb060fa2355e23a29985f
dragokillfist <tinker198431@gmail.com>
2025-04-22 13:29:21 -0500

    initial implement of Sol Charm

--- Content/Items/Charms/CharmSol.cs
+++ Content/Items/Charms/CharmSol.cs
@@ -1,9 +1,15 @@
-using SpectreMod.Content.Materials;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Terraria;
+using Terraria.Net;
+using Terraria.GameContent;
 using Terraria.GameContent.Bestiary;
+using Terraria.GameContent.NetModules;
 using Terraria.ID;
 using Terraria.ModLoader;
-using Terraria.GameContent;
+using Terraria.ModLoader.IO;
 
 namespace SpectreMod.Content.Items.Charms
 {
@@ -21,13 +27,73 @@ namespace SpectreMod.Content.Items.Charms
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
             player.GetDamage(DamageClass.Generic) *= 1.9f;
-            player.GetModPlayer<CharmSolPlayer>().intensity = 1;
+            player.GetModPlayer<CharmSolPlayer>().MeleeSize = 1;
+            player.GetModPlayer<CharmSolPlayer>().PlayerSpeed = 1;
+            player.GetModPlayer<CharmSolPlayer>().MountSpeed = 1;
+            player.GetModPlayer<CharmSolPlayer>().IsActive = true;
         }
     }
     
     public class CharmSolPlayer : ModPlayer
     {
-        public int intensity;
-       
+        public int KillCount;
+        public int KillNumRequired = 100;
+        public int intensity = 0;
+        public int MountSpeed = 0;
+        public int MountSpeedMod = 1;
+        public int PlayerSpeed = 0;
+        public int PlayerSpeedMod = 1;
+        public int MeleeSize = 0;
+        public int MeleeSizeMod = 1;
+        public int scalemod = 0;
+        public bool IsActive = false;
+        
+        public void RegisterKill(NPC npc)
+        {
+            if (npc.type == NPCID.SolarDrakomire)
+            {
+                KillCount++;
+                if (KillCount >= KillNumRequired)
+                {
+                    PlayerSpeedMod++;
+                    MountSpeed++;
+                    MeleeSizeMod++;
+                    if (KillNumRequired >= 1000)
+                    {
+                        KillNumRequired += 1000;
+                    }
+                    else
+                    {
+                        KillNumRequired += 100;
+                    }
+                }
+            }
+        }
+        public override void ModifyItemScale(Item item, ref float scale)
+        {
+            base.ModifyItemScale(item, ref scale);
+            if (IsActive)
+            {
+            scale *= MeleeSize + (MeleeSizeMod / 10f);
+            }
+            else
+            {
+                scale = 1;
+            }
+            scalemod = (int)scale;
+        }
+        
+        public override void PostUpdateRunSpeeds()
+        {
+            base.PostUpdateRunSpeeds();
+            Player.accRunSpeed *= 1 + PlayerSpeedMod;
+        }
+        public override void ResetEffects()
+        {
+            base.ResetEffects();
+            PlayerSpeed = 0;
+            MountSpeed = 0;
+            IsActive = false;
+        }
     }
 }
