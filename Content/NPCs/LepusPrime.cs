e9b5abfcf208c79d73530bb5db6dea9a9062fdf0
normalcat <tntgav@gmail.com>
2025-04-23 16:31:28 -0700

    lepus prime more ai - forgot to commit last night

--- Content/NPCs/LepusPrime.cs
+++ Content/NPCs/LepusPrime.cs
@@ -10,6 +10,7 @@ using Terraria.ModLoader;
 
 namespace SpectreMod.Content.NPCs
 {
+    [AutoloadBossHead]
     public class LepusPrime : ModNPC
     {
         public override string Texture => "SpectreMod/Common/PlaceHolders/PlaceHolder";
@@ -26,8 +27,14 @@ namespace SpectreMod.Content.NPCs
             NPC.width = 20;
             NPC.height = 20;
 
-            NPC.defense = 45;
+            NPC.defense = 10;
             NPC.damage = 125;
+            NPC.boss = true;
+            
+            NPCID.Sets.DontDoHardmodeScaling[Type] = true;
+           
+
+            data = [0, 0, 0, 0, 0];
         }
 
         public static Vector2 Up(float amt) { return new Vector2(0, -amt); }
@@ -36,22 +43,46 @@ namespace SpectreMod.Content.NPCs
         public static Vector2 Right(float amt) { return new Vector2(amt, 0); }
         public static Vector2 Tiles(Vector2 toTile) { return toTile * 16; }
 
+        public int[] data;
+        public float gravityMult = 1f;
+
         public override void AI()
         {
             Timer++;
+            NPC.GravityMultiplier *= gravityMult;
             float distance;
             Player player = Main.player[NPC.FindClosestPlayer(out distance)];
             Vector2 plrpos = player.position;
 
-            if (Timer % 60 == 0)
+            if (Timer % 120 == 0)
             {
                 int chosenProjectile = ProjectileID.DeathLaser;
-                int speed = 15;
+                int speed = 10;
                 int spawnDistance = 70;
                 Projectile.NewProjectile(NPC.GetSource_FromAI(), plrpos + Tiles(Left(spawnDistance)), Right(speed), chosenProjectile, QuarterDamage, 0);
                 Projectile.NewProjectile(NPC.GetSource_FromAI(), plrpos + Tiles(Right(spawnDistance)), Left(speed), chosenProjectile, QuarterDamage, 0);
                 Projectile.NewProjectile(NPC.GetSource_FromAI(), plrpos + Tiles(Up(spawnDistance)), Down(speed), chosenProjectile, QuarterDamage, 0);
                 Projectile.NewProjectile(NPC.GetSource_FromAI(), plrpos + Tiles(Down(spawnDistance)), Up(speed), chosenProjectile, QuarterDamage, 0);
+            }
+
+
+
+            if (data[0] <= 0 && NPC.collideY)
+            {
+                
+                data[0] = Main.rand.Next(75, 220);
+                gravityMult = 0.01f;
+                //jump code
+                Vector2 normalizedDiff = plrpos - NPC.position;
+                normalizedDiff.Normalize();
+
+                NPC.velocity += normalizedDiff * (float)Math.Pow(distance, 0.35f);
+
+            } else { data[0] = data[0] - 1; }
+
+            if (data[0] <= 50)
+            {
+                gravityMult = 1;
             }
         }
     }
