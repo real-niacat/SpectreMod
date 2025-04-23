41043c714852d79ea0e9f1bacf1706b3a2e6bedd
dragokillfist <tinker198431@gmail.com>
2025-04-22 19:43:26 -0500

    removed unneeded files and now only 1 normal charm can be equiped

--- Content/Items/Charms/CharmDistraught_Base.cs
+++ Content/Items/Charms/CharmDistraught_Base.cs
@@ -24,14 +24,31 @@ namespace SpectreMod.Content.Items.Charms
             Item.accessory = true;
         }
         
-        public virtual bool CanAccessoryBeEquippedWith(Item equippedItem, Item itemToEquip, Player player, AccessorySlotType context)
+        public override bool CanAccessoryBeEquippedWith(Item equippedItem, Item incomingItem, Player player)
         {
-            if (equippedItem.ModItem is CharmDistraught_Base && equippedItem != itemToEquip)
+            if (equippedItem.ModItem is CharmDistraught_Upgraded && equippedItem != incomingItem)
+            {
+                return false; // Prevents equipping multiple charms of the same type
+            }
+            else if (equippedItem.ModItem is CharmDistraught_UpgradedPlus && equippedItem != incomingItem)
+            {
+                return false; // Prevents equipping multiple charms of the same type
+            }
+            else if (equippedItem.ModItem is CharmProgress_Base && equippedItem != incomingItem)
+            {
+                return false; // Prevents equipping multiple charms of the same type
+            }
+            else if (equippedItem.ModItem is CharmProgress_Upgraded && equippedItem != incomingItem)
+            {
+                return false; // Prevents equipping multiple charms of the same type
+            }
+            else if (equippedItem.ModItem is CharmLunacy && equippedItem != incomingItem)
             {
                 return false; // Prevents equipping multiple charms of the same type
             }
             return true; // Allows equipping other charms or items
         }
+        
 
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
