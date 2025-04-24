1115a734a97c64e841c81a889ae522df35b98840
normalcat <tntgav@gmail.com>
2025-04-23 22:15:50 -0700

    i genuinely can't keep my head up im sotired just habe the changes incase you needthem

--- Content/NPCs/LepusPrime.cs
+++ Content/NPCs/LepusPrime.cs
@@ -42,7 +42,15 @@ namespace SpectreMod.Content.NPCs
         public static Vector2 Left(float amt) { return new Vector2(-amt, 0); }
         public static Vector2 Right(float amt) { return new Vector2(amt, 0); }
         public static Vector2 Tiles(Vector2 toTile) { return toTile * 16; }
-        public Vector2 Towards(Vector2 target) { Vector2 c = (target - NPC.position); c.Normalize(); return c; }
+        public static float Radians(float degrees) { return (float)(degrees * Math.PI / 180); }
+        public static Vector2 RotateCW(Vector2 from, float angle) 
+        {
+            double newX = (from.X * Math.Cos(Radians(angle))) - (from.Y * Math.Sin(Radians(angle)));
+            double newY = (from.X * Math.Sin(Radians(angle))) + (from.Y * Math.Cos(Radians(angle)));
+            return new Vector2((float)newX, (float)newY);
+        }
+        public Vector2 Towards(Vector2 target) { Vector2 c = target - NPC.position; c.Normalize(); return c; }
+
 
         public int[] data;
         public float gravityMult = 1f;
@@ -71,7 +79,7 @@ namespace SpectreMod.Content.NPCs
 
 
             if (NPC.velocity.X > 10) { NPC.velocity.X *= 0.99f; }
-            if (NPC.collideY) { NPC.velocity.X += Towards(plrpos).X; NPC.velocity.X *= 0.95f; }
+            if (NPC.collideY) { NPC.velocity.X += Towards(plrpos).X; NPC.velocity.X *= 0.93f; }
 
             if (data[0] <= 0 && (NPC.collideX || NPC.collideY || distance > 80))
             {
@@ -81,11 +89,15 @@ namespace SpectreMod.Content.NPCs
                 //jump code
                 Vector2 normalizedDiff = plrpos - NPC.position;
                 normalizedDiff.Normalize();
+                normalizedDiff.Y *= 2;
                 float spd = Math.Max(15, (float)Math.Pow(distance, 0.45f));
 
                 NPC.velocity += normalizedDiff * spd;
 
-                Projectile.NewProjectile(NPC.GetSource_FromAI(), NPC.Center, Towards(plrpos) * spd * 1.5f, ProjectileID.BombSkeletronPrime, QuarterDamage, 0);
+                for (int i = 0; i < 120; i += 15)
+                {
+                    Projectile.NewProjectile(NPC.GetSource_FromAI(), NPC.Center, RotateCW(Towards(plrpos), i-60) * spd * 1.5f, ProjectileID.DeathLaser, QuarterDamage, 0);
+                }
 
             } else { data[0] = data[0] - 1; }
         }
