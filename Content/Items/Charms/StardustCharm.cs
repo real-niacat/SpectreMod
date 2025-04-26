d4f3ec546b1e99a12818a5ae816f007ef0fea08e
dragokillfist <tinker198431@gmail.com>
2025-04-26 14:07:23 -0500

    balancing

--- Content/Items/Charms/StardustCharm.cs
+++ Content/Items/Charms/StardustCharm.cs
@@ -26,6 +26,7 @@ namespace SpectreMod.Content.Items.Charms
     {
         internal const long BaseLevelCost = 400000L;
         internal static long LevelCost(int level) => BaseLevelCost * level;
+        internal static long CumulativeLevelCost(int level) => (BaseLevelCost / 2L) * level * (level + 1);
         internal const int MaxLevel = 60; // was 60.
 
         internal const float ModifierAmount = 0.5f;
@@ -58,7 +59,7 @@ namespace SpectreMod.Content.Items.Charms
             CharmStardustPlayer charmStardustPlayer = player.GetModPlayer<CharmStardustPlayer>();
             charmStardustPlayer.charmStardust = this;
             player.GetDamage(DamageClass.Summon) *= 1.9f;
-            player.maxMinions = 1 + level / 2;
+            player.maxMinions += 1 + (level / 2);
             player.statManaMax2 += level * 5;
             player.statMana += level * 5;
             player.GetModPlayer<CharmStardustPlayer>().IsActive = true;
@@ -92,8 +93,29 @@ namespace SpectreMod.Content.Items.Charms
             TooltipLine line = new TooltipLine(Mod, "Level", $"Level: {level}/{MaxLevel}");
             line.OverrideColor = Color.LimeGreen;
             tooltips.Add(line);
-            line = new TooltipLine(Mod, "Damage", $"Current Level Progress: {totalDamageModifier}/{LevelCost(level + 1)}");
-            line.OverrideColor = Color.LimeGreen;
+            string ProgressKey = "[PROGRESS]";
+            TooltipLine progressLine = tooltips.FirstOrDefault(x => x.Mod == "Terraria" && x.Text.Contains(ProgressKey));
+            if (progressLine != null)
+            {
+                if (level < MaxLevel)
+                {
+                    long progressToNextLevel = totalDamageModifier - CumulativeLevelCost(level);
+                    long totalToNextLevel = LevelCost(level + 1);
+                    double ratio = (double)progressToNextLevel / totalToNextLevel;
+                    string percent = (100D * ratio).ToString("0.00");
+                    progressLine.Text = progressLine.Text.Replace(ProgressKey, percent);
+                }
+                else
+                {
+                    progressLine.Text = string.Empty;
+                }
+                string damageKey = "[SummonDmgInc]";
+                TooltipLine damageLine = tooltips.FirstOrDefault(x => x.Mod == "Terraria" && x.Text.Contains(damageKey));
+                if (damageLine != null)
+                {
+                    damageLine.Text = damageLine.Text.Replace(damageKey, $"{(level * 0.1f) * 100}%");
+                }
+            }
             line = new TooltipLine(Mod, "Level", $"Bonus Minion Slots: {level}");
             line.OverrideColor = Color.Magenta;
             tooltips.Add(line);
@@ -108,15 +130,18 @@ namespace SpectreMod.Content.Items.Charms
         public int SlotMod;
         public int ManaMod;
         
+        /// <summary>
+        /// <para/> Adappted code from <see href="https://github.com/CalamityTeam/CalamityModPublic/blob/1.4.4/Items/Accessories/ShatteredCommunity.cs">The Shattered Community</see> from the Calamity Mod.
+        /// </summary>
         internal void AccumulateDamageModifier(long damage)
-        {
+        { 
             if (charmStardust is null)
                 return;
             SlotModifier = charmStardust.level;
             ManaBoost = charmStardust.level * 5;
             if (IsActive)    
                 charmStardust.totalDamageModifier += damage;
-            if (IsActive && charmStardust.level < StardustCharm.MaxLevel && charmStardust.totalDamageModifier > StardustCharm.LevelCost(charmStardust.level + 1))
+            if (IsActive && charmStardust.level < StardustCharm.MaxLevel && charmStardust.totalDamageModifier > StardustCharm.CumulativeLevelCost(charmStardust.level + 1))
             {
                 charmStardust.level++;
                 charmStardust.totalDamageModifier = 0L;
